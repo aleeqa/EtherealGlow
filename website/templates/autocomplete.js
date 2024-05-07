b84f0c82a013c4c9432ad405e68c556cccc31a9d
jasdevsingh05 <jasdevsingh5768@gmail.com>
2024-05-07 17:32:43 +0800

    Updated search bar.

---
+++ website/templates/autocomplete.js
@@ -0,0 +1,18 @@
+let availableKeywords =[
+    'My account',
+    'About us',
+    'My skincare',
+    'Blog',
+
+];
+
+const resultsBox = document.querySelector(".result-box");
+const inputbox = document.getElementById("input-box")
+
+inputBox.onkeyup = function(){
+    let result = [];
+    let input = inputBox
+  
+
+
+}
